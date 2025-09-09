ade1f4fd0d6a95b804ab15882411207bc447097c
Ahmad Dev <se231310@fhstp.ac.at>
2025-09-09 15:35:20 +0200

    Added core information and sources

---
+++ src/main/java/com/example/demo/Medien.java
@@ -0,0 +1,38 @@
+public abstract class Medium {
+    private String title;
+    private int copies;
+
+    protected Medium(String title, int copies) {
+        this.title = title;
+        this.copies = copies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public boolean inStock() {
+        return copies > 0;
+    }
+
+    public void takeOne() {
+        if (copies <= 0) throw new IllegalStateException("No copies left!");
+        copies--;
+    }
+
+    public void returnOne() {
+        copies++;
+    }
+}
