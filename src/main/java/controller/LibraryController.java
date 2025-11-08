3553e5b5eba70687bdb03affd947c35e48e34e9c
Ahmad Dev <se231310@fhstp.ac.at>
2025-11-08 19:23:27 +0100

    minor update

--- src/main/java/controller/LibraryController.java
+++ src/main/java/controller/LibraryController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 import java.util.UUID;
 
 @RestController
--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,7 +1,5 @@
 package model;
 
-import model.MediaItem;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
